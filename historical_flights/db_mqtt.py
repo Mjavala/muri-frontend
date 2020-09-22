754515dc89a6ef2329821f879a36637f8a3eebcf
Jose Bravo <mjabravo221@gmail.com>
2020-09-22 16:35:55 -0700

    fix: channel switch

--- historical_flights/db_mqtt.py
+++ historical_flights/db_mqtt.py
@@ -84,7 +84,7 @@ class muri_app_mqtt():
         # need to call the the status function in main every second
         payload = json.loads(str(message.payload.decode()))
 
-        if message.topic == 'muri_stat/stat':
+        if message.topic == 'muri_test/stat':
             if self.live:
                 self.clear_raw_data()
                 self.rssi = payload['receiver_1']['last']['rssi_last']['rssi']
@@ -93,7 +93,7 @@ class muri_app_mqtt():
                 self.station = payload['station']
                 self.stats()
         #self.msg_to_db_raw = payload
-        if message.topic == 'muri_stat/raw':
+        if message.topic == 'muri_test/raw':
             if payload['data']['frame_data']:
                 result = self.simulation_check(payload['data']['ADDR_FROM'])
                 if result:
