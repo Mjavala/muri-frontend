616d1b59de7712b4cf3c0eae8ebf47a08aec747d
Mjavala <mjabravo221@gmail.com>
2020-11-02 22:32:29 -0600

    fix: logging

--- historical_flights/logs.py
+++ historical_flights/logs.py
@@ -3,25 +3,28 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 def main_app_logs():
-    logger = logging.getLogger("db")
+    try:
+        logger = logging.getLogger("db")
 
-    logging.basicConfig(
-        level=logging.ERROR,
-        format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-        filemode="a",
-    )
-    # log_file = "~/muri/db.log".format()
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    print(logger)
-    print(logger.hasHandlers)
-    if not logger.hasHandlers():
-        file_handler = logging.handlers.TimedRotatingFileHandler(
-            "/root/muri/db.log", when="h", backupCount=24
+        logging.basicConfig(
+            level=logging.ERROR,
+            format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            filemode="a",
         )
-        file_handler.setFormatter(formatter)
-        file_handler.setLevel(logging.INFO)
-        logger.addHandler(file_handler)
+        # log_file = "~/muri/db.log".format()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        print(logger)
+        print(logger.hasHandlers)
+        if not logger.hasHandlers():
+            file_handler = logging.handlers.TimedRotatingFileHandler(
+                "/root/muri/db.log", when="h", backupCount=24
+            )
+            file_handler.setFormatter(formatter)
+            file_handler.setLevel(logging.INFO)
+            logger.addHandler(file_handler)
 
-    return logging.getLogger("db")
+        return logging.getLogger("db")
+    except Exception as e:
+        print("LOGGING SETUP EXCEPTION{}".format(e)
