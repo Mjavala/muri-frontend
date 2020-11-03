61834dc3ac98fc82b20b181cf23d7b016cb2caf8
Mjavala <mjabravo221@gmail.com>
2020-11-02 22:29:51 -0600

    fix: logging

--- historical_flights/main.py
+++ historical_flights/main.py
@@ -31,32 +31,34 @@ db_node = db.muri_db()
 
 
 async def main_loop():
-    logger = logs.main_app_logs()
-    print(logger)
-    logger.info("Starting MURI App Main Program...")
+    try:
+        logger = logs.main_app_logs()
+        print(logger)
+        logger.info("Starting MURI App Main Program...")
 
-    if not os.path.exists(path):
-        os.mkdir(path)
-        logger.info("Logging directory made")
+        if not os.path.exists(path):
+            os.mkdir(path)
+            logger.info("Logging directory made")
 
-    logger.info("Starting MURI database service...")
+        logger.info("Starting MURI database service...")
 
-    # queue = mqtt_conn.get_queue()
-    qo = mqtt_conn.get_q_out()
-    # qi = mqtt_conn.get_q_in()
-    # q_db_stat = db_node.get_stat_q()
-    # q_db_0xc = db_node.get_0xc_q()
-    # q_db_0xd = db_node.get_0xd_q()
-    while True:
-        if not qo.empty():
-            val = qo.get_nowait()
-            db_node.add_to_queue(val)
+        # queue = mqtt_conn.get_queue()
+        qo = mqtt_conn.get_q_out()
+        # qi = mqtt_conn.get_q_in()
+        # q_db_stat = db_node.get_stat_q()
+        # q_db_0xc = db_node.get_0xc_q()
+        # q_db_0xd = db_node.get_0xd_q()
+        while True:
+            if not qo.empty():
+                val = qo.get_nowait()
+                db_node.add_to_queue(val)
 
-        if qo.qsize() > 100:
-            await asyncio.sleep(0.3)
-        else:
-            await asyncio.sleep(2)
-
+            if qo.qsize() > 100:
+                await asyncio.sleep(0.3)
+            else:
+                await asyncio.sleep(2)
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
 
